5b365a63294acbb45c723fcfea937522a6c47495
Red Gogh <redgogh@vip.qq.com>
2025-03-13 15:16:20 +0800

    init

---
+++ numcp.h
@@ -0,0 +1,145 @@
+/* -------------------------------------------------------------------------------- *\
+|*                                                                                  *|
+|*    Copyright (C) 2019-2024 RedGogh All rights reserved.                          *|
+|*                                                                                  *|
+|*    Licensed under the Apache License, Version 2.0 (the "License");               *|
+|*    you may not use this file except in compliance with the License.              *|
+|*    You may obtain a copy of the License at                                       *|
+|*                                                                                  *|
+|*        http://www.apache.org/licenses/LICENSE-2.0                                *|
+|*                                                                                  *|
+|*    Unless required by applicable law or agreed to in writing, software           *|
+|*    distributed under the License is distributed on an "AS IS" BASIS,             *|
+|*    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.      *|
+|*    See the License for the specific language governing permissions and           *|
+|*    limitations under the License.                                                *|
+|*                                                                                  *|
+\* -------------------------------------------------------------------------------- */
+#ifndef MATH_H_
+#define MATH_H_
+
+#include "vec.h"
+
+#define NAN     __builtin_nanf("")
+#define PI      (3.14159265358979323846F)
+
+float fmod (float x, float y)
+{
+        if (y == 0)
+                return 0.0f;
+
+        return x - ((int) (x / y)) * y; // NOLINT(*-narrowing-conversions)
+}
+
+float abs(float a)
+{
+        return a < 0.0f ? -a : a;
+}
+
+float pow(float x, int a)
+{
+        if (a < 2)
+                return x;
+
+        float q = 1.0f;
+
+        while ((a--) > 0)
+                q *= x;
+
+        return q;
+}
+
+int degrees(float angrad)
+{
+        return (int) (angrad * (180.0f / PI));
+}
+
+float radians(int angdeg)
+{
+        return (float) angdeg * (PI / 180.0f);
+}
+
+float sin(float a)
+{
+        a = fmod(a, 2 * PI);
+
+        float q = a;
+        float t = a;
+        int sign = 1;
+        int limit = a < 1.0f ? 16 : 32;
+
+        for (int n = 3; n <= limit; n += 2) {
+                t *= (a * a) / (float) (n * (n - 1));
+                q += (float) (sign *= -1) * t;
+        }
+
+        return q;
+}
+
+float cos(float a)
+{
+        a = fmod(a, 2 * PI);
+
+        float q = 1.0f;
+        float t = 1.0f;
+        int sign = 1;
+        int limit = a < 1.0f ? 16 : 32;
+
+        for (int n = 2; n <= limit; n += 2) {
+                t *= (a * a) / (float) (n * (n - 1));
+                q += (float) (sign *= -1) * t;
+        }
+
+        return q;
+}
+
+float sqrt(float x, float tol = 1e-10, int max = 1024)
+{
+        if (x < 0)
+                return NAN;
+
+        float fx = x;
+
+        for (int i = 0; i < max; i++) {
+                float n = 0.5f * (fx + x / fx);
+
+                if (abs(n - fx) < tol)
+                        return n;
+
+                fx = n;
+        }
+
+        return fx;
+}
+
+float len(const vec2 &vec)
+{
+        return sqrt((vec.x * vec.x) + (vec.y * vec.y));
+}
+
+float len(const vec3 &vec)
+{
+        return sqrt((vec.x * vec.x) + (vec.y * vec.y) + (vec.z * vec.z));
+}
+
+float len(const vec4 &vec)
+{
+        return sqrt((vec.x * vec.x) + (vec.y * vec.y) + (vec.z * vec.z) + (vec.w * vec.w));
+}
+
+float dot(const vec2 &a, const vec2 &b)
+{
+        return (a.x * b.x) + (a.y * b.y);
+}
+
+float dot(const vec3 &a, const vec3 &b)
+{
+        return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
+}
+
+float dot(const vec4 &a, const vec4 &b)
+{
+        return (a.x * b.x) + (a.y * b.y) + (a.z * b.z) + (a.w * b.w);
+}
+
+#endif /* MATH_H_ */
---
+++ vec.h
@@ -0,0 +1,144 @@
+/* -------------------------------------------------------------------------------- *\
+|*                                                                                  *|
+|*    Copyright (C) 2019-2024 RedGogh All rights reserved.                          *|
+|*                                                                                  *|
+|*    Licensed under the Apache License, Version 2.0 (the "License");               *|
+|*    you may not use this file except in compliance with the License.              *|
+|*    You may obtain a copy of the License at                                       *|
+|*                                                                                  *|
+|*        http://www.apache.org/licenses/LICENSE-2.0                                *|
+|*                                                                                  *|
+|*    Unless required by applicable law or agreed to in writing, software           *|
+|*    distributed under the License is distributed on an "AS IS" BASIS,             *|
+|*    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.      *|
+|*    See the License for the specific language governing permissions and           *|
+|*    limitations under the License.                                                *|
+|*                                                                                  *|
+\* -------------------------------------------------------------------------------- */
+#ifndef MATH_VEC_H_
+#define MATH_VEC_H_
+
+#define FNC_OPERATOR_VEC2_IMPLEMENTS(op)                                                                \
+        __vec2_t<T, N>                                                                                  \
+            operator op (T scalar)                                                                      \
+                {                                                                                       \
+                   return __vec2_t<T, N>(x op scalar, y op scalar);                                     \
+                };                                                                                      \
+                                                                                                        \
+        __vec2_t<T, N>                                                                                  \
+            operator op (const __vec2_t<T, N> &vec)                                                     \
+                {                                                                                       \
+                   return __vec2_t<T, N>(x op vec.x, y op vec.y);                                       \
+                }
+
+#define FNC_OPERATOR_VEC3_IMPLEMENTS(op)                                                                \
+        __vec3_t<T, N>                                                                                  \
+            operator op (T scalar)                                                                      \
+                {                                                                                       \
+                   return __vec3_t<T, N>(x op scalar, y op scalar, z op scalar);                        \
+                };                                                                                      \
+                                                                                                        \
+        __vec3_t<T, N>                                                                                  \
+            operator op (const __vec3_t<T, N> &vec)                                                     \
+                {                                                                                       \
+                   return __vec3_t<T, N>(x op vec.x, y op vec.y, z op vec.z);                           \
+                }
+
+#define FNC_OPERATOR_VEC4_IMPLEMENTS(op)                                                                \
+        __vec4_t<T, N>                                                                                  \
+            operator op (T scalar)                                                                      \
+                {                                                                                       \
+                   return __vec4_t<T, N>(x op scalar, y op scalar, z op scalar, w op scalar);           \
+                };                                                                                      \
+                                                                                                        \
+        __vec4_t<T, N>                                                                                  \
+            operator op (const __vec4_t<T, N> &vec)                                                     \
+                {                                                                                       \
+                   return __vec4_t<T, N>(x op vec.x, y op vec.y, z op vec.z, w op vec.w);               \
+                }
+
+template<typename T, int N = 2>
+struct __vec2_t {
+        static constexpr T fV = T(0.0f);
+
+        union {
+                struct { T x, y; };
+                struct { T r, g; };
+
+                /* data pointer */
+                T array[N];
+        };
+
+        explicit __vec2_t()         : __vec2_t(fV) {}
+        explicit __vec2_t(T scalar) : __vec2_t(scalar, scalar) {}
+        explicit __vec2_t(T x, T y) : r(x), g(y) {}
+
+        FNC_OPERATOR_VEC2_IMPLEMENTS(+);
+        FNC_OPERATOR_VEC2_IMPLEMENTS(-);
+        FNC_OPERATOR_VEC2_IMPLEMENTS(*);
+        FNC_OPERATOR_VEC2_IMPLEMENTS(/);
+
+};
+
+template<typename T, int N = 4>
+struct __vec3_t {
+        static constexpr T fV = T(0.0f);
+
+        union {
+                struct { T x, y, z; };
+                struct { T r, g, b; };
+
+                /* data pointer */
+                T array[N];
+        };
+
+        explicit __vec3_t()              : __vec3_t(fV) {}
+        explicit __vec3_t(T scalar)      : __vec3_t(scalar, scalar, scalar) {}
+        explicit __vec3_t(T x, T y, T z) : r(x), g(y), b(z) {}
+
+        FNC_OPERATOR_VEC3_IMPLEMENTS(+);
+        FNC_OPERATOR_VEC3_IMPLEMENTS(-);
+        FNC_OPERATOR_VEC3_IMPLEMENTS(*);
+        FNC_OPERATOR_VEC3_IMPLEMENTS(/);
+
+};
+
+template<typename T, int N = 3>
+struct __vec4_t {
+        static constexpr T fV = T(0.0f);
+
+        union {
+                struct { T x, y, z, w; };
+                struct { T r, g, b, a; };
+
+                /* data pointer */
+                T array[N];
+        };
+
+        explicit __vec4_t()                   : __vec4_t(fV) {}
+        explicit __vec4_t(T scalar)           : __vec4_t(scalar, scalar, scalar, scalar) {}
+        explicit __vec4_t(T x, T y, T z, T w) : r(x), g(y), b(z), a(w) {}
+
+        FNC_OPERATOR_VEC4_IMPLEMENTS(+);
+        FNC_OPERATOR_VEC4_IMPLEMENTS(-);
+        FNC_OPERATOR_VEC4_IMPLEMENTS(*);
+        FNC_OPERATOR_VEC4_IMPLEMENTS(/);
+
+};
+
+typedef __vec2_t<float> vec2;
+typedef __vec3_t<float> vec3;
+typedef __vec4_t<float> vec4;
+
+#define FNC_VECTOR_POINTER_IMPLEMENTS(tp)               \
+    template<typename T>                                \
+        inline static T* getptr(tp<T> &vec)             \
+          {                                             \
+            return &vec.array[0];                       \
+          }
+
+FNC_VECTOR_POINTER_IMPLEMENTS(__vec2_t)
+FNC_VECTOR_POINTER_IMPLEMENTS(__vec3_t)
+FNC_VECTOR_POINTER_IMPLEMENTS(__vec4_t)
+
+#endif /* MATH_VEC_H_ */
